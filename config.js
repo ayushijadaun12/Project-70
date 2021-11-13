35d9ef21eb88f0d7600730c4bb53f91e3e763adc
ayushijadaun12 <94225606+ayushijadaun12@users.noreply.github.com>
2021-11-13 17:28:44 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDm3NQvDT5MYdrjnzog9-r7gif6kjLOySE",
+    authDomain: "project-70-80286.firebaseapp.com",
+    projectId: "project-70-80286",
+    storageBucket: "project-70-80286.appspot.com",
+    messagingSenderId: "478901923597",
+    appId: "1:478901923597:web:81e31c070116170e498a44"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
